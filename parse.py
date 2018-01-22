796403ac1aeae95824c46b2503d87c3df32cba1c
Bagus Maulana <bmaulana273@gmail.com>
2018-01-22 17:44:31 +0000

    cleanup

--- parse.py
+++ parse.py
@@ -10,6 +10,7 @@ matcher = Matcher(nlp.vocab)
 print('Loading SpaCy model took', time.time() - start_time, 'seconds')
 
 
+# TODO output to an out file instead of console
 def main(fname):
     """
     Format: python parse.py filename(without .json)
@@ -23,6 +24,7 @@ def main(fname):
     f_in = open(path, 'r')
     for line in f_in:
         js = json.loads(line)
+        print(list(js.keys())[0])  # TODO replace with title
         text = list(js.values())[0]['text']
         doc = nlp(text)
         matches = matcher(doc)
@@ -30,15 +32,16 @@ def main(fname):
         for match_id, start, end in matches:
             token = doc[start]
             print(token.text, start)
+            # TODO instead of just using direct parent/child, use their parent/child as well,
+            # but decrease weight of sentiment depending on how 'far' the word is
             if token.dep_ != 'ROOT':
                 print('Parent', token.head.text, token.head.sentiment)
             for child in token.children:
                 print('Child', child.text, child.sentiment)
                 # TODO SpaCy's sentiment analyser isn't good enough.
                 # e.g. words with obvious sentiment (e.g. issues, disorder) has sentiment of 0.0
-                # Implement some other sentiment analyser and pass it the token's text instead.
+                # Implement some other sentiment data set and pass it the token's text instead.
             print()
-        print()
 
 
 if __name__ == '__main__':
