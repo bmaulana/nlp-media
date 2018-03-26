b88e2da2352f6355d9ba930939a4e7a9721512b2
Bagus Maulana <bmaulana273@gmail.com>
2018-03-26 22:16:06 +0100

    sentiment-eval outputs to json
    TODO: read labels from 'saved' JSON

--- sentiment-eval.py
+++ sentiment-eval.py
@@ -13,10 +13,13 @@ def eval_sentiment(num_samples=5):
     mean_scores = np.array([[0.0, 0.0, 0.0]] * len(labels))
     all_scores = [([], [], []) for i in range(len(labels))]
     answers = []  # to save answers so results are reproducible
-    out_file = './out-sentiment/eval.csv'
     num_sents = 0
 
+    if not os.path.exists('./out-eval/'):
+        os.makedirs('./out-eval/')
+
     # From each file in ./out-sentiment/, show a sample of num_sents sentences to manually classify
+    f_out = open('./out-eval/eval.json', 'w', encoding='utf-8')
     for filename in os.listdir('./out-sentiment'):
         f_in = open('./out-sentiment/' + filename, 'r', encoding='utf-8')
         sents = {}
@@ -42,6 +45,7 @@ def eval_sentiment(num_samples=5):
             print(scores)
             print()
             if res == '+':
+                # positive
                 for i in range(len(labels)):
                     full_label = 'sentiment_score_' + labels[i]
                     current_score = max(-1.0, min(1.0, float(scores[full_label])))
@@ -49,13 +53,18 @@ def eval_sentiment(num_samples=5):
                     all_scores[i][0].append(current_score)
                 answers.append((sent, '+'))
             elif res == '-':
+                # negative
                 for i in range(len(labels)):
                     full_label = 'sentiment_score_' + labels[i]
                     current_score = max(-1.0, min(1.0, float(scores[full_label])))
                     mean_scores[i, 2] += float(scores[full_label])
                     all_scores[i][2].append(current_score)
                 answers.append((sent, '-'))
+            elif res == 'o':
+                # out of topic
+                continue
             else:
+                # neutral
                 for i in range(len(labels)):
                     full_label = 'sentiment_score_' + labels[i]
                     current_score = max(-1.0, min(1.0, float(scores[full_label])))
@@ -64,6 +73,14 @@ def eval_sentiment(num_samples=5):
                 answers.append((sent, 'n'))
             num_sents += 1
 
+            to_write = {'sentence': sent, 'label': answers[-1][1]}
+            for i in range(len(labels)):
+                full_label = 'sentiment_score_' + labels[i]
+                to_write[full_label] = scores[full_label]
+            f_out.write(json.dumps(to_write))
+            f_out.write('\n')
+    f_out.close()
+
     # Print mean scores for each classification, for each sentiment scorer
     mean_scores /= num_sents
     for i in range(len(labels)):
@@ -71,10 +88,12 @@ def eval_sentiment(num_samples=5):
               '\tNegative:', mean_scores[i][2])
 
     # output sentences and answers for reproducibility
-    f_out = open(out_file, 'w', encoding='utf-8')
+    f_out = open('./out-eval/eval.csv', 'w', encoding='utf-8')
     for answer in answers:
         f_out.write(str(answer[1]) + ';' + str(answer[0]) + '\n')
     f_out.close()
+
+    # TODO output to json with label AND scores for each link
 
     # Plot histogram for each classification, for each sentiment scorer
     fig, axs = plt.subplots(2, (len(labels) + 1) // 2, figsize=(5 * ((len(labels) + 1) // 2), 10), tight_layout=True)
@@ -87,8 +106,25 @@ def eval_sentiment(num_samples=5):
         ax.set_xlabel('Sentiment Score')
         ax.set_ylabel('No. of articles')
         ax.legend()
-    plt.savefig('./out-sentiment/eval.png')
+    plt.savefig('./out-eval/eval.png')
     plt.close()
+
+
+def json_to_list(fname):
+    f_in = open('./out-eval/eval.json', 'r', encoding='utf-8')
+    json_list = []
+    for line in f_in:
+        json_list.append(json.loads(line))
+    f_in.close()
+    f_out = open('./out-eval/eval.json', 'w', encoding='utf-8')
+    f_out.write(json.dumps(json_list))
+    f_out.close()
+    pass
+
+
+# TODO function that reads a labelled JSON file with +/-/n and scores for each scorer
+def eval_sentiment_read(fname):
+    pass
 
 
 if __name__ == '__main__':
